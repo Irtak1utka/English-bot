095b2803909851bf57794e5ca9e3688a9ec0b76a
Alex <alex.surikov@inbox.ru>
2025-05-05 23:17:32 +0300

    Попытка создать заучивание №2 Теперь хотя бы кнопки появляются Чуть позже будет доведено до рабочего состояния

--- src/заучивание.py
+++ src/заучивание.py
@@ -2,7 +2,9 @@ import telebot
 from telebot import types
 import json
 import random
+
 from main import bot_token
+
 
 bot = telebot.TeleBot(bot_token)
 learning_mode = False
@@ -19,24 +21,42 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет')
 
 
-def random_word_from_list():
+def random_word_from_list(message):
     global button_list
     transtation_word = random.choice(list(dictionary.keys()))
     button_list.append(transtation_word)
     bot.send_message(message.chat.id, text=transtation_word)
+    print(transtation_word)
 
     button_list.append(dictionary[transtation_word])
     c_distionary = list(dictionary.values()).copy()
-    other_3_word = random.choices(c_distionary, 3)
+    other_3_word = random.choices(c_distionary, k=3)
     button_list += other_3_word
     random.shuffle(button_list)
 
+    answer_is_correct(message)
 
-@bot.message_handler(commands=['cards'])
+
+def answer_is_correct(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for el in button_list:
+        item_i = types.KeyboardButton(el)
+        markup.add(item_i)
+    bot.send_message(message.chat.id, 'Выберите верный вариант', reply_markup=markup)
+
+
+@bot.message_handler(commands=['learn'])
 def try_card(message):
     global learning_mode
     learning_mode = True
-    random_word_from_list()
+    random_word_from_list(message)
+
+
+@bot.message_handler(commands=['cards'])
+def cards(message):
+    for el in dictionary:
+        bot.send_message(message.chat.id, f"{el} - <tg-spoiler>{dictionary[el]}</tg-spoiler>")
+        print(el)
 
 
 @bot.message_handler(content_types='text')
@@ -46,17 +66,12 @@ def probably(message):
         if message == "stop":
             learning_mode = False
             exit()
-        else:
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            for el in button_list:
-                item_i = types.KeyboardButton(el)
-                markup.add(item_i)
-    # for i in range(3):
-    #     button_list.append(random.choice(list(dictionary.keys())))
-    # random.shuffle(button_list)
-    # for i in range(4):
-    #     item_i = types.KeyboardButton(dictionary[button_list[i]])
-    #     markup.add(item_i)
+        # else:
+            # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            # for el in button_list:
+            #     item_i = types.KeyboardButton(el)
+            #     markup.add(item_i)
+            # bot.send_message(message.chat.id, 'Выберите верный вариант', reply_markup=markup)
 
 
 bot.infinity_polling()
